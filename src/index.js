a7a1f4906a088525135568cc610ea155d2eee4de
sideroad <sideroad.jp@gmail.com>
2017-01-21 23:38:28 +0900

    Export Chips

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import Input from './Input';
 import InputtableButton from './InputtableButton';
 import Cards from './Cards';
+import Chips from './Chips';
 
-export { Button, Input, InputtableButton, Cards };
+export { Button, Input, InputtableButton, Cards, Chips };
